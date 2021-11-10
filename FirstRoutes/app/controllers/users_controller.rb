c4694ef67ec110af0ee58d07676cc5bba09c0c2b
calebjo <caleb.ri.jones@gmail.com>
2021-11-10 16:15:31 -0500

    Change users_controller.rb

--- FirstRoutes/app/controllers/users_controller.rb
+++ FirstRoutes/app/controllers/users_controller.rb
@@ -1,16 +1,22 @@
 class UsersController < ApplicationController
+    
+    def create
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        render json: params
+    end
+
     def index
         # render plain: "HELLO"
         
         @users = User.all
         render json: @users
-    end
-
-    def create
-        render json: params
-    end
-
-    def show
-        render json: params
     end
 end
