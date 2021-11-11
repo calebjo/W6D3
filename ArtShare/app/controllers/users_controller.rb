b491cdf383dedf03765a355e2bfa309de8fba80d
Josh <joshualaikowski@gmail.com>
2021-11-11 10:48:49 -0500

    Pushing

--- ArtShare/app/controllers/users_controller.rb
+++ ArtShare/app/controllers/users_controller.rb
@@ -5,4 +5,27 @@ class UsersController < ApplicationController
         render json: @users
     end
 
+    def create
+        user = User.new(params.require(:user).permit(:username))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    def update
+    end
+
+    private
+    # def params
+    #     params.require(:user).permit(:username)
+    # end
+
 end
