e8777cc25e314b5f5fedc6019711762652230512
Josh <joshualaikowski@gmail.com>
2021-11-11 11:29:17 -0500

    Screen Swap

--- ArtShare/config/routes.rb
+++ ArtShare/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :create, :show, :destroy, :update]
+  resources :artworks, only: [:index, :create, :show, :destroy, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
